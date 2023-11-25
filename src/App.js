a3625a446378590473f366f390d6a59c3764fdbb
Brandon McPhatter <brandonmcphatter1@gmail.com>
2023-11-25 05:07:16 -0600

    name search functional

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import './App.css';
 import {useEffect, useState} from "react";
+import * as PropTypes from "prop-types";
 
-function App() {
-    const [query, setQuery] = useState('');
+
+export default function App() {
+    const [query, setQuery] = useState('Lebron');
     const [teams, setTeams] = useState([]);
+    const [players, setPlayers] = useState([]);
 
 
-    function handleSubmit() {
-        console.log(teams[0])
-    }
+
+    console.log(players);
 
     return (
         <div className="App">
             <h1>The NBA App</h1>
             <input type="text" value={query} name='searchBar' onChange={(e) => setQuery(e.target.value)}/>
-            <button type='submit' onClick={handleSubmit}>Search</button>
-
+            <button type='submit'>Search</button>
+            <PlayerList players={players} setPlayers={setPlayers} query={query}/>
             <TeamList teams={teams} setTeams={setTeams}/>
         </div>
     );
@@ -49,5 +51,37 @@ function TeamList({teams, setTeams}) {
     );
 }
 
+function PlayerList({players, setPlayers, query}) {
+    useEffect(() => {
+        async function getPlayers() {
+            const response = await fetch(`https://www.balldontlie.io/api/v1/players?search=${query}`);
+            const data = await response.json();
+            setPlayers(data.data);
+        }
 
-export default App;
+        getPlayers();
+    }, [query, setPlayers]);
+
+    if (players) {
+        return (
+            <div>
+                {players.map(({first_name, last_name, id, team}) => (
+                    <div className='playerInfo' key={id}>
+                        <h5>
+                            {first_name} {last_name}
+                        </h5>
+                        <h4>
+                            {team.full_name}
+                        </h4>
+                    </div>
+                ))}
+            </div>
+        );
+    }
+    return null;
+}
+
+PlayerList.propTypes = {
+    setPlayers: PropTypes.func,
+    players: PropTypes.arrayOf(PropTypes.any)
+};
